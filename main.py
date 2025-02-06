94f59f3ef883340e10582f1743f3ca230232c18a
bogdan <147440132+bogdanswag@users.noreply.github.com>
2025-02-07 01:43:15 +0300

    Final version

--- main.py
+++ main.py
@@ -5,24 +5,24 @@ import asyncio
 
 
 async def main():
-    tdataFolder = r"C:\Users\root\progs\Portable\Telegram\tdata"
+    tdataFolder = r"path\to\Portable\Telegram\tdata"
     tdesk = TDesktop(tdataFolder)
 
-    assert tdesk.isLoaded(), "Ошибка загрузки tdata. Убедитесь, что путь указан верно."
+    assert tdesk.isLoaded(), "Failed loading tdata. Check tdataFolder path."
 
     client = await tdesk.ToTelethon(session="telethon.session", flag=UseCurrentSession)
 
     await client.connect()
 
     if not await client.is_user_authorized():
-        print("Не удалось авторизоваться. Проверьте данные tdata.")
+        print("Authorization failed. Check your tdata.")
         return
 
-    bot_username = "woodyvpn_bot"
-    start_parameter = "prettyboyclique"
+    bot_username = "referal_bot_username"
+    start_parameter = "todo_telegram_account_username"
 
     await client.send_message(bot_username, f"/start {start_parameter}")
-    print("Сообщение отправлено боту!")
+    print("Message successfully sent!")
 
     await client.disconnect()
 
